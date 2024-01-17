aa9704a0f69faaf63fae45f09c099d6dc7f2f9a1
Robert Jandow <robert.jandow@tum.de>
2024-01-17 14:57:51 +0100

    Add priority queue for repository cloning

---
+++ HadesCloneContainer/pq.go
@@ -0,0 +1,50 @@
+package main
+
+import "container/heap"
+
+/// Based on the example from https://pkg.go.dev/container/heap#example-package-PriorityQueue
+
+type Item struct {
+	Repository
+	order int
+	index int
+}
+
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	// We want Pop to give us the lowest order number
+	return pq[i].order < pq[j].order
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x any) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() any {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // avoid memory leak
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+// update modifies the priority and value of an Item in the queue.
+func (pq *PriorityQueue) update(item *Item, value Repository, order int) {
+	item.Repository = value
+	item.order = order
+	heap.Fix(pq, item.index)
+}
