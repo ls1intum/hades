37935f3dbfa1b2b4b303b90a6674c36628ae41bc
Robert Jandow <38583713+robertjndw@users.noreply.github.com>
2025-05-20 11:07:03 +0200

    Switch from Redis to NATS  (#2)
    * Refactor job queue handling to support priority-based processing and update related tests
    
    * Enhance job stream configuration to disallow duplicates
    
    * Add KeyValue store for job management
    
    * Add Bruno files for NATS

--- HadesAPI/monitoring.go
+++ HadesAPI/monitoring.go
@@ -3,21 +3,23 @@ package main
 import (
 	"encoding/json"
 
-	"github.com/hibiken/asynqmon"
 	"github.com/ls1intum/hades/shared/payload"
 	log "github.com/sirupsen/logrus"
 )
 
-var MetadataObfuscator = asynqmon.PayloadFormatterFunc(func(_ string, bytes []byte) string {
+// SafePayloadFormatter formats a job payload for display, removing sensitive metadata
+func SafePayloadFormatter(bytes []byte) string {
 	var job payload.QueuePayload
 	if err := json.Unmarshal(bytes, &job); err != nil {
 		log.WithError(err).Error("Failed to unmarshal task payload")
 		return ""
 	}
+
+	// Remove sensitive metadata from job steps
 	for i := range job.Steps {
 		job.Steps[i].Metadata = make(map[string]string)
 	}
 
 	json_tmp, _ := json.Marshal(job)
 	return string(json_tmp)
-})
+}
--- shared/utils/config.go
+++ shared/utils/config.go
@@ -9,10 +9,11 @@ import (
 	"github.com/joho/godotenv"
 )
 
-type RedisConfig struct {
-	Addr        string `env:"REDIS_ADDR,notEmpty" envDefault:"localhost:6379"`
-	Pwd         string `env:"REDIS_PWD"`
-	TLS_Enabled bool   `env:"REDIS_TLS_ENABLED" envDefault:"false"`
+type NatsConfig struct {
+	URL      string `env:"NATS_URL,notEmpty" envDefault:"nats://localhost:4222"`
+	Username string `env:"NATS_USERNAME"`
+	Password string `env:"NATS_PASSWORD"`
+	TLS      bool   `env:"NATS_TLS_ENABLED" envDefault:"false"`
 }
 
 type ExecutorConfig struct {
