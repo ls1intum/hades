85b1e61cfb5723c68fbf4d49925625bc7909765b
Robert Jandow <38583713+robertjndw@users.noreply.github.com>
2026-02-19 12:57:42 +0100

    Move api from logrus to slog (#370)
    * Avoid variable shadowing in TestAddBuildToQueueRoute
    
    * Refactor logging to use slog
    
    * Implement CodeRabbit Feedback and improve code
    
    * Update go.sum to include missing module entries

--- HadesAPI/router_test.go
+++ HadesAPI/router_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"log/slog"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -20,7 +21,6 @@ import (
 	"github.com/testcontainers/testcontainers-go/wait"
 
 	hadesnats "github.com/ls1intum/hades/shared/nats"
-	log "github.com/sirupsen/logrus"
 )
 
 const NATS_IMAGE = "nats:2.11.4"
@@ -50,13 +50,13 @@ func (suite *APISuite) SetupSuite() {
 		Started:          true,
 	})
 	if err != nil {
-		log.Fatalf("Could not start NATS: %s", err)
+		slog.Error("Could not start NATS", "error", err)
 	}
 
 	endpoint, err := suite.natsC.Endpoint(ctx, "")
-	log.Infof("NATS endpoint: %s", endpoint)
+	slog.Info("NATS endpoint", "endpoint", endpoint)
 	if err != nil {
-		log.Fatalf("Could not get NATS endpoint: %s", err)
+		slog.Error("Could not get NATS endpoint", "error", err)
 	}
 
 	// Setup NATS connection
@@ -68,13 +68,13 @@ func (suite *APISuite) SetupSuite() {
 
 	suite.natsConnection, err = hadesnats.SetupDefaultNatsConnection(natsConfig)
 	if err != nil {
-		log.Fatalf("Failed to connect to NATS: %v", err)
+		slog.Error("Failed to connect to NATS", "error", err)
 	}
 
 	// Create producer for tests
 	suite.hadesProducer, err = hadesnats.NewHadesPublisher(suite.natsConnection)
 	if err != nil {
-		log.Fatalf("Failed to create HadesProducer: %v", err)
+		slog.Error("Failed to create HadesProducer", "error", err)
 	}
 
 	// Set the global HadesProducer for tests
@@ -90,7 +90,7 @@ func (suite *APISuite) TearDownSuite() {
 	// Stop NATS container
 	ctx := context.Background()
 	if err := suite.natsC.Terminate(ctx); err != nil {
-		log.Fatalf("Could not stop NATS: %s", err)
+		slog.Error("Could not stop NATS", "error", err)
 	}
 }
 
@@ -192,6 +192,6 @@ func (suite *APISuite) TestInvalidJSON() {
 }
 
 func TestAPISuite(t *testing.T) {
-	log.SetLevel(log.DebugLevel)
+	slog.SetLogLoggerLevel(slog.LevelDebug)
 	suite.Run(t, new(APISuite))
 }
--- HadesScheduler/go.mod
+++ HadesScheduler/go.mod
@@ -39,6 +39,7 @@ require (
 	github.com/containerd/platforms v0.2.1 // indirect
 	github.com/ebitengine/purego v0.8.4 // indirect
 	github.com/fxamacker/cbor/v2 v2.9.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3 // indirect
 	github.com/moby/go-archive v0.1.0 // indirect
 	github.com/moby/sys/userns v0.1.0 // indirect
 	github.com/nats-io/nkeys v0.4.11 // indirect
@@ -49,16 +50,14 @@ require (
 	github.com/spf13/pflag v1.0.9 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.33.0 // indirect
-	go.opentelemetry.io/proto/otlp v1.4.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.34.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.5.0 // indirect
 	go.yaml.in/yaml/v2 v2.4.3 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/net v0.47.0 // indirect
 	golang.org/x/oauth2 v0.30.0 // indirect
 	golang.org/x/term v0.37.0 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20241209162323-e6fa225c2576 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20241209162323-e6fa225c2576 // indirect
-	google.golang.org/grpc v1.68.1 // indirect
+	google.golang.org/grpc v1.72.1 // indirect
 	gopkg.in/evanphx/json-patch.v4 v4.13.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
